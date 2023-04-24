1be6cd819cb9c50e94914d528bc8b4fc4faee936
Nam Le <namhoangle236@gmail.com>
2023-04-23 22:18:29 -0700

    Week 1 assignment
    week 1 assignment, including screenshot

---
+++ Week_1_Assignment/app.js
@@ -0,0 +1,9 @@
+alert("Hello! Welcome to Nam Le's IT-121 Website!");
+console.log("hello, can you see this in the console?");
+
+let a = 5;
+a++;
+let b = 2;
+let total = a + b;
+
+console.log(total);
